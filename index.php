5fc07c147e0176785f20348ba471eefd26cd21b2
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-01-18 16:39:34 +0100

    WSP-2 add correct env connection

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    include_once(__DIR__ . '/vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    print_r($_ENV);
+    ?>
 </body>
 </html>
