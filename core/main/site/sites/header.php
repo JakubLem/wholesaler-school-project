5bac5f6604a51f0242044f165cef76025e94460c
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-01-19 10:22:14 +0100

    WSP-9 edit structure and corrections

--- core/main/site/sites/header.php
+++ core/main/site/sites/header.php
@@ -6,16 +6,5 @@
     <title>Document</title>
 </head>
 <body>
-    testtest
-    <header></header>
-    testtest
-    <?php
-        $GLOBALS['db']->make_query("SELECT * FROM olejki");
-        $result = $GLOBALS['db']->get_last_response();
-        while($row = $result->fetch_assoc()) {
-            print_r($row);
-            echo "<br>";
-        }
-    ?>
 </body>
 </html>
