7f870afa2b6d78f6e01730b3fa49fe15ceb44d82
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-01-25 11:35:09 +0100

    WSP-10 js update

--- core/main/header.php
+++ core/main/header.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <a href="/wholesaler-school-project/core/main/main_page.php" class="active" onclick="test()">Strona główna</a>
+    <a href="/wholesaler-school-project/core/main/main_page.php" class="active" onclick="header(1)">Strona główna</a>
     <a href="/wholesaler-school-project/core/main/products.php">Nasze towary</a>
     <a>Kontakt</a>
 </div>
--- core/main/scripts/script.js
+++ core/main/scripts/script.js
@@ -1,5 +1,8 @@
 console.log(" xd x d xd");
-
+function header(param) {
+    console.log(param);
+    let header = document.getElementsByClassName("header");
+}
 function test() {
     console.log("test");
 }
