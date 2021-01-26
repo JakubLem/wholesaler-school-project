5a1b2a0276afa53b61f0f489d32bdc4adc3feabb
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-01-26 10:29:42 +0100

    WSP-10 update JS

--- core/main/scripts/script.js
+++
@@ -1,9 +0,0 @@
-console.log(" xd x d xd");
-function header(param) {
-    console.log(param);
-    let header = document.getElementsByClassName("header");
-
-}
-function test() {
-    console.log("test");
-}
