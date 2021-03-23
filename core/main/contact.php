2b92edded936a256c942a706aecf887c4f2f64b3
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-03-23 20:52:52 +0100

    WSP-30 create contact form

--- core/main/contact.php
+++ core/main/contact.php
@@ -1,5 +1,18 @@
 <?php
 $GLOBALS['header'] = 4;
 @include_once(__DIR__. '/start.php');
+?>
+<link rel="stylesheet" href="/wholesaler-school-project/core/main/styles/contact.css">
+<link rel="stylesheet" href="/wholesaler-school-project/core/main/styles/account.css">
+<?php
 @include_once(__DIR__. '/top.php');
+?>
+<form class="register-form-form-parent" action="forms/contact_message.php" method="post">
+    <input class="register-form-form" type="text" name="user_email" placeholder="Adres email">
+    <input class="register-form-form" type="text" name="title" placeholder="Temat">
+    <textarea class="message-register-form-form" type="" name="message" placeholder="Treść wiadomości"></textarea>
+    <input class="register-form-form" type="submit" value="Wyślij">
+</form>
+<?php
 @include_once(__DIR__. '/stop.php');
+?>
