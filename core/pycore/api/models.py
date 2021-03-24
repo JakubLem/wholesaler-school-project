29b87c89256b6d1b5ba71b3d279492bc92d60951
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-03-24 20:37:01 +0100

    WSP-21 update models

--- core/pycore/api/models.py
+++ core/pycore/api/models.py
@@ -8,13 +8,14 @@ class Note(models.Model):
         return f'{self.string}'
 
 
+class PriceList(models.Model):
+    quantity = models.IntegerField()
+
+
 class Option(models.Model):
     max_weight = models.FloatField()
     price = models.FloatField()
+    price_list = models.ForeignKey(PriceList, null=False, blank=False, on_delete=models.CASCADE, related_name='options')
 
     def __str__(self):
         return f'{self.max_weight, self.price}'
-
-
-class PriceList(models.Model):
-    quantity = models.IntegerField()
