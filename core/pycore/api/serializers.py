de99fc50ba6166f8ad306cf39ac5968182cb9535
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-03-26 09:09:23 +0100

    WSP-38 update serializers

--- core/pycore/api/serializers.py
+++ core/pycore/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 from .models import Note, PriceList, Option
 
 
@@ -10,7 +11,7 @@ class NoteSerializer(serializers.ModelSerializer):
 
 
 class PriceListSerializer(serializers.ModelSerializer):
-    options = serializers.SlugRelatedField(many=True, read_only=True, slug_field='id')
+    options = serializers.SlugRelatedField(many=True, read_only=True, slug_field='main_identifier')
     
     class Meta:
         model = PriceList
@@ -18,6 +19,19 @@ class PriceListSerializer(serializers.ModelSerializer):
 
 
 class OptionSerializer(serializers.ModelSerializer):
+    price_list = serializers.CharField(max_length=150)
+
     class Meta:
         model = Option
-        fields = ('id', 'max_weight', 'price')
+        fields = ('id', 'max_weight', 'price', 'price_list')
+
+    def validate(self, data):
+        try:
+            price_list = get_object_or_404(PriceList, main_identifier=data['price_list'])
+        except Exception:
+            raise ValidationError("PriceList with identifier {identifier} doesn't exist".format(
+                identifier=data['price_list']
+            ))
+        data['price_list'] = price_list
+
+        return data
