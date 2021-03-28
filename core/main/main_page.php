c9318b4088615ff05bc81e4d1e9888170f96171b
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-03-28 18:46:18 +0200

    WSP-32 front update

--- core/main/main_page.php
+++ core/main/main_page.php
@@ -1,5 +1,23 @@
 <?php
 $GLOBALS['header'] = 1;
 @include_once(__DIR__. '/start.php');
+?>
+<link rel="stylesheet" href="/wholesaler-school-project/core/main/styles/mainpage.css">
+<?php
 @include_once(__DIR__. '/top.php');
+?>
+
+<div class="center-text-animated"><p>Jasteśmy najpopularniejszym dostawcą towarów w Europie!</p></div>
+
+<div class="main-photo">
+    <img class="photo" src="/wholesaler-school-project/core/main/files/photos/mainimg.jpg">
+    <div class="description">
+        <p>
+            Witaj! <br>
+            Na rynku działamy ponad 35 lat. Dostarczamy produkty do ponad 40 państw w Europie.
+        </p>    
+    </div>
+</div>
+
+<?php
 @include_once(__DIR__. '/stop.php');
