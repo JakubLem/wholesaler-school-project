37d44b5f9bc6e001d9b64d09b61ab0a2017b2160
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-03-28 18:26:26 +0200

    WSP-42 create style for price list

--- core/main/pricelist.php
+++ core/main/pricelist.php
@@ -1,6 +1,9 @@
 <?php
 $GLOBALS['header'] = 3;
 @include_once(__DIR__. '/start.php');
+?>
+<link rel="stylesheet" href="/wholesaler-school-project/core/main/styles/pricelist.css">
+<?php
 @include_once(__DIR__. '/top.php');
 
 $curl = curl_init();
@@ -31,7 +34,20 @@ $options = get_all_price_list_options($options_data);
 if(count($options) == 0) {
     // TODO WSP-43 validation
 } else {
-    
+    ?>
+        <div class="price-list-header"><h1>Cennik transportu</h1></div>
+        <div class="price-list-option">
+            <div class="max_weight">WAGA DO</div>
+            <div class="price">CENA</div>
+        </div>
+
+    <?php
+        foreach ($options as &$option) {
+            echo '<div class="price-list-option">';
+            echo '<div class="max_weight">'.$option->max_weight.'</div>';
+            echo '<div class="price">'.$option->price.'</div>';
+            echo '</div>';
+        }
 }
 
 @include_once(__DIR__. '/stop.php');
