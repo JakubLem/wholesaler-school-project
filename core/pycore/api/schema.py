f10e8b3ecc8874f7540df8d452984c82a9e2b26d
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-03-28 14:30:59 +0200

    WSP-40 try to update graphql

--- core/pycore/api/schema.py
+++ core/pycore/api/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-
+import django_filters
 from .models import Note, Option, PriceList
 
 
@@ -16,16 +16,47 @@ class OptionType(DjangoObjectType):
         fields = ("max_weight", "price")
 
 
+class PriceListFilter(django_filters.FilterSet):
+    class Meta:
+        model = PriceList
+        fields = ['main_identifier']
+
 class PriceListType(DjangoObjectType):
     class Meta:
         model = PriceList
-        fields = ("main_identifier", "options")
+        # filterset_class = PriceListFilter
+        # interfaces = (graphene.relay.Node, )
+        fields = ("id", "main_identifier", "options")
 
 
 class Query(graphene.ObjectType):
     notes = graphene.List(NoteType)
+    options = graphene.List(OptionType)
+    pricelists = graphene.List(PriceListType)
+    # pricelist = graphene.relay.Node.Field(PriceListType)
 
     def resolve_notes(self, info):
         return Note.objects.all()
 
+    def resolve_options(self, info):
+        return Option.objects.all()
+
+    def resolve_pricelists(self, info, **args):
+        main_identifier = args.get('main_identifier')
+        print(main_identifier)
+        return PriceList.objects.get(main_identifier=main_identifier)
+
 schema = graphene.Schema(query=Query)
+
+
+"""
+query {
+    pricelists {
+        mainIdentifier,
+        options {
+            maxWeight
+            price
+        }
+    }
+}
+"""
