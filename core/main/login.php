db17a042d2a7f60214f84454be43905f52bb1957
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-02 20:01:33 +0200

    WSP-45 change type of input

--- core/main/login.php
+++ core/main/login.php
@@ -1,5 +1,5 @@
 <form name="login-form" class="register-form-form-parent" action="forms/user_login_form.php" method="post" onsubmit="return login_validate()">
-    <input class="register-form-form" type="text" id="login_user_email" name="login_user_email" placeholder="Adres email">
+    <input class="register-form-form" type="email" id="login_user_email" name="login_user_email" placeholder="Adres email">
     <input class="register-form-form" type="password" id="login_user_password" name="login_user_password" placeholder="Hasło">
     <input class="register-form-form" type="submit" value="Logowanie">
     <p id="login_error_response"></p>
