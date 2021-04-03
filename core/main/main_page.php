a66127cdc0e2506b28545ce69f8a52e6ed527ecc
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-03 15:28:30 +0200

    WSP-32 add animations

--- core/main/main_page.php
+++ core/main/main_page.php
@@ -7,21 +7,39 @@ $GLOBALS['header'] = 1;
 @include_once(__DIR__. '/top.php');
 ?>
 
-<div class="center-text-animated"><p>Jasteśmy najpopularniejszym dostawcą towarów w Europie!</p></div>
+<div class="center-text-animated"><h1>Hurtownia Dostarcz Kamienia!</h1>
+<p>Jasteśmy najpopularniejszym dostawcą towarów w Europie!</p></div>
 
 <div class="main-photo">
     <img class="photo" src="/wholesaler-school-project/core/main/files/photos/businessman.jpg">
     <div class="description">
-        <p>
-            Witaj! <br>
-            Na rynku działamy ponad 35 lat. Dostarczamy produkty do ponad 40 państw w Europie.
-        </p>    
+        <p class="description-description">Zadowoleni klienci:</p>
+        <figure class="chart" data-percent="98">
+            <figcaption>98%</figcaption>
+            <svg width="200" height="200">
+            <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+            </svg>
+        </figure>
+        <p class="description-description">Dostarczamy do 35 państw Europy!</p>
+        <figure class="chart" data-percent="75">
+            <figcaption>74,8%</figcaption>
+            <svg width="200" height="200">
+            <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+            </svg>
+        </figure>
+        <p class="description-description">Obsługujemy 11267 firm!</p>
+        <figure class="chart" data-percent="100">
+            <figcaption>74,8%</figcaption>
+            <svg width="200" height="200">
+            <circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
+            </svg>
+        </figure>
     </div>
 </div>
 <div class="main-photo">
-    <div class="description">
+    <div class="main-description">
         <p>
-            Witaj! <br>
+            Witaj <br>
             Na rynku działamy ponad 35 lat. Dostarczamy produkty do ponad 40 państw w Europie.
         </p>    
     </div>
