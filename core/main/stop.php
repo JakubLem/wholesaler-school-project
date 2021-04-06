04141bac9490337995e7bae4d16e11774b98e80f
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-06 16:54:24 +0200

    WSP-51 try to connect php

--- core/main/stop.php
+++ core/main/stop.php
@@ -1,5 +1,22 @@
-<script src="scripts/script.js"></script>
-<div id="fixedContainer"><img width="80" height="80" src="/wholesaler-school-project/core/main/files/photos/disability.jpg" onclick="ability_switch()"></div>
+<script>
+function ability_switch() {
+    console.log(ABILITY_CHECKER);
+    if(ABILITY_CHECKER == 1){
+        console.log("XD");
+        document.body.classList.remove("ability-class-on");
+        <?php
+            $_SESSION['ability_checker'] = 0;
+        ?>
+    } else {
+        document.body.classList.add("ability-class-on");
+        <?php
+            $_SESSION['ability_checker'] = 1;
+        ?>
+    }
+}
+</script>
+
+<div id="fixedContainer"><img width="80" height="80" src="/wholesaler-school-project/core/main/files/photos/disability.jpg" onclick="ability_switch()">
 </body>
 </html>
 <?php
--- core/main/top.php
+++ core/main/top.php
@@ -5,7 +5,11 @@
 @include_once('connect_db.php');
 
 @include_once(__DIR__. '/header.php');
+if(!isset($_SESSION['ability_checker'])) {
+    $_SESSION['ability_checker'] = 0;
+}
 ?>
+
 <script>
-    let ABILITY_CHECKER = false;
+    let ABILITY_CHECKER = <?php echo $_SESSION['ability_checker']; ?> ;
 </script>
