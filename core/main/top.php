77c4c8078f2ee7f4364960fbbed80b1b0cd4a643
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-06 17:13:16 +0200

    WSP-51 delete php

--- core/main/top.php
+++ core/main/top.php
@@ -5,11 +5,8 @@
 @include_once('connect_db.php');
 
 @include_once(__DIR__. '/header.php');
-if(!isset($_SESSION['ability_checker'])) {
-    $_SESSION['ability_checker'] = 0;
-}
 ?>
 
 <script>
-    let ABILITY_CHECKER = <?php echo $_SESSION['ability_checker']; ?> ;
+    let ABILITY_CHECKER = false;
 </script>
