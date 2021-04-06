aa80f1f4ec7eae068fb4486de72bf3923c5c1f64
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-06 16:44:50 +0200

    WSP-51 add functions

--- core/main/stop.php
+++ core/main/stop.php
@@ -1,4 +1,5 @@
 <script src="scripts/script.js"></script>
+<div id="fixedContainer"><img width="80" height="80" src="/wholesaler-school-project/core/main/files/photos/disability.jpg" onclick="ability_switch()"></div>
 </body>
 </html>
 <?php
--- core/main/top.php
+++ core/main/top.php
@@ -6,3 +6,6 @@
 
 @include_once(__DIR__. '/header.php');
 ?>
+<script>
+    let ABILITY_CHECKER = false;
+</script>
