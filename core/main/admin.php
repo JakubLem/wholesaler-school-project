d4744d8864bfb8f5339a63b3dc4956dd9855db79
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2021-04-08 21:45:56 +0200

    WSP-58 init admin login

---
+++ core/main/admin.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS['header'] = 1;
+@include_once(__DIR__. '/start.php');
+?>
+<link rel="stylesheet" href="/wholesaler-school-project/core/main/styles/products.css">
+<?php
+@include_once('connect_db.php');
+@include_once('admin_start_header.php');
