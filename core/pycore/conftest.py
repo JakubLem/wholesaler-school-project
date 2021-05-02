f0d53d8c7c0432f202879955f555e0ab2eb3841d
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-05-02 13:39:06 +0200

    WSP-71 init fixture

--- core/pycore/conftest.py
+++ core/pycore/conftest.py
@@ -3,7 +3,12 @@
 import pytest
 from django.test import Client
 from graphene_django.utils.testing import graphql_query
+
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
+
 from django.utils.crypto import get_random_string
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 @pytest.fixture
@@ -31,3 +36,18 @@ def notes():
         return result
 
     return array
+
+
+@pytest.fixture(scope='function')
+def temp_xlsx_file():
+
+    def temp_file(header, data):
+        wb = Workbook()
+        ws = wb.active
+        ws.append(header)
+        for row in data:
+            ws.append(row)
+        file_obj = save_virtual_workbook(wb)
+        return SimpleUploadedFile("test_file.xlsx", file_obj, content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+    return temp_file
