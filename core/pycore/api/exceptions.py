3998c6b1bbe2390af417e132e8784fbb6cb44f40
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-05-08 00:31:21 +0200

    WSP-60 update oop

--- core/pycore/api/exceptions.py
+++ core/pycore/api/exceptions.py
@@ -1,8 +1,28 @@
 from rest_framework.exceptions import APIException
 
+
+class Error:
+    def __init__(self, category, message):
+
+
+class Errors:
+    def __init__(self, errors):
+        self.errors = errors
+
+    @property
+    def json(self):
+        return self.errors
+
+
 class GeneralException(APIException):
-    pass
+    def __init__(self, detail=None, code=None):
+        super(GeneralException, self).__init__(detail=detail, code=code)
+        self.code = code
+        self.detail = detail
 
 
 class InvalidRequest(GeneralException):
-    pass
+    status_code = 400
+    
+    def __init__(self, errors):
+        self.errors = errors
